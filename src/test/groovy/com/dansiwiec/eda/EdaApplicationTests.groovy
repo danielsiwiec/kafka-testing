314dd181893ea0af096f2c49b58a86e863e0706d
Dan Siwiec <daniel.siwiec@gmail.com>
2020-05-09 12:06:17 -0700

    Rewrite tests to Spock

--- src/test/groovy/com/dansiwiec/eda/EdaApplicationTests.groovy
+++ src/test/groovy/com/dansiwiec/eda/EdaApplicationTests.groovy
@@ -1,20 +1,20 @@
 package com.dansiwiec.eda
 
 
-import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.kafka.test.context.EmbeddedKafka
+import spock.lang.Specification
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @EmbeddedKafka(topics = ["orders"],
         bootstrapServersProperty = "spring.kafka.bootstrap-servers",
-        brokerProperties = ["log.dir=/tmp/out/embedded-kafka"])
-class EdaApplicationTests {
+        brokerProperties = ["log.dir=build/embedded-kafka"])
+class EdaApplicationTests extends  Specification {
 
     @LocalServerPort
     private int port;
@@ -22,15 +22,16 @@ class EdaApplicationTests {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Test
-    void createsOrder() {
+    def "should create an order"() {
+        given:
         def url = "http://localhost:$port/orders"
-
-        def order1 = new OrderRequest(itemId: "321", customerId: "123")
-        restTemplate.postForObject(url, order1, Order).with {
+        def request = new OrderRequest(itemId: "321", customerId: "123")
+        when:
+        def order = restTemplate.postForObject(url, request, Order)
+        then:
+        order.with {
             assert id == "0"
             assert items == ["321"]
         }
     }
-
 }
